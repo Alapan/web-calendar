4373f4330c4b0335045a0e0597a5e621979eac20
Alapan Mukherjee <alapan.mukherjee@medisapiens.fi>
2016-12-18 17:46:16 +0200

    Add constants file

---
+++ src/app/constants.js
@@ -0,0 +1,25 @@
+export const daysOfWeek = {
+  0: 'Sun',
+  1: 'Mon',
+  2: 'Tues',
+  3: 'Wed',
+  4: 'Thurs',
+  5: 'Fri',
+  6: 'Sat'
+};
+
+export const monthsOfYear = {
+  0: 'Jan',
+  1: 'Feb',
+  2: 'Mar',
+  3: 'Apr',
+  4: 'May',
+  5: 'Jun',
+  6: 'Jul',
+  7: 'Aug',
+  8: 'Sept',
+  9: 'Oct',
+  10: 'Nov',
+  11: 'Dec'
+};
+
