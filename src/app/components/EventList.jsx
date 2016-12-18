93e9a6a983907861a663307fd4c593d6f41d3c07
Alapan Mukherjee <alapan.mukherjee@medisapiens.fi>
2016-12-18 17:45:09 +0200

    Show event details

--- src/app/components/EventList.jsx
+++ src/app/components/EventList.jsx
@@ -1,34 +1,56 @@
 import React from 'react';
 const $ = require('jquery');
+import { daysOfWeek, monthsOfYear } from '../constants';
 
 export default class EventList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: ''
+    };
+  }
 
-    constructor() {
-        super();
-        this.events = [];
-    }
+  componentWillMount() {
+    $.get('http://localhost:3000/events', (data) => {
+      this.setState({
+        events: data
+      });
+    });
+  }
 
-    componentWillMount() {
-        const url = window.location.origin + '/events/';
-        $.ajax({
-            url,
-            datatype: 'json',
-            cache: false,
-            success: (data) => {
-                console.log(data);
-                this.events = data;
-            }
-        });
-    }
+  showDateAndTime(row) {
+    const startDate = new Date(row.start_date);
+    const endDate = new Date(row.end_date);
+    let day = '';
+    let month = '';
+    let date = '';
 
-    render() {
-        return (
-            <ul className="list-group">
-                { this.events.map((event) => {
-                      return <li className="list-group-item">{ event }</li>;
-                  })
-                }
-            </ul>
-        );
+    // If start and end dates are the same
+    if (startDate.getTime() === endDate.getTime()) {
+      const d = new Date(startDate);
+      day =  daysOfWeek[d.getDay()];
+      month = monthsOfYear[d.getMonth()];
+      date = d.getDate();
     }
+    return (<b>{`${day}\t${month}\t${date}\t${row.start_time}\t-\t${row.end_time}`}</b>);
+  }
+
+  render() {
+    if (this.state.events) {
+      return (
+        <ul className="list-group">
+          {this.state.events.map((row, i) => {
+            return (
+              <li key={i} className="list-group-item">
+                <h3>{row.name}</h3>
+                {this.showDateAndTime(row)}
+                <p>{row.description}</p>
+              </li>
+            )
+          })}
+        </ul>
+      )
+    }
+    return null;
+  }
 }
